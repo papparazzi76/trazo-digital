5f3552266c7fa5038b2ae7cf49121e99e0f99fb3
papparazzi76 <carlos@arcasl.es>
2025-08-05 13:51:21 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,19 @@
+import HeroSection from "@/components/sections/HeroSection";
+import ProjectsGallery from "@/components/sections/ProjectsGallery";
+import ServicesSection from "@/components/sections/ServicesSection";
+import PricingSection from "@/components/sections/PricingSection";
+import TestimonialsSection from "@/components/sections/TestimonialsSection";
+import CTASection from "@/components/sections/CTASection";
 
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <ProjectsGallery />
+      <ServicesSection />
+      <PricingSection />
+      <TestimonialsSection />
+      <CTASection />
+    </>
+  );
+}
